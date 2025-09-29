66d0054f5636c987af8b8cf0fe717456f288274e
sanaz dolati <sanazdolati32@gmail.com>
2025-09-29 16:13:39 +0330

    Update App.vue to import TodoList component

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,8 @@
-import {createApp} from 'vue' import App from './App.vue'
-createApp(App).mount('#app')
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+ <TodoList />
+  </div>
+</template>
+<script setup>
+import TodoList from "./components/TodoList.vue"
+</script setup>
